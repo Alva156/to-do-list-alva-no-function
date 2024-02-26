ff2881fda73838457695fd69a8db5310f5eaa473
Alva156 <andreicimoune.alvarico.cics@ust.edu.ph>
2024-02-26 13:47:09 +0800

    duummy items

--- src/localStorage.js
+++ src/localStorage.js
@@ -3,6 +3,21 @@ export const getLocalStorage = () => {
   if (list) {
     return JSON.parse(list);
   } else {
-    return [];
+    return [
+      { id: new Date().getTime().toString(), title: "Buy Mom and Dad gifts" },
+      {
+        id: new Date().getTime().toString() + 1,
+        title: "Basketball at Kapatiran",
+      },
+      {
+        id: new Date().getTime().toString() + 1,
+        title: "Eat Hard Boiled Eggs",
+      },
+      {
+        id: new Date().getTime().toString() + 1,
+        title: "Basketball at San Mateo",
+      },
+      { id: new Date().getTime().toString() + 1, title: "Sleep @9pm" },
+    ];
   }
 };
