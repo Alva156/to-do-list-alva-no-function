75086cf41134592d98efd4ac460c698e826fb162
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-04-08 14:05:35 +0800

    sorting options

--- src/App.js
+++ src/App.js
@@ -7,34 +7,60 @@ import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
 
 import { getLocalStorage } from "./localStorage";
+// const dummyTasks = [
+//   { id: "1", title: "Buy Mom and Dad gifts" },
+//   { id: "2", title: "Basketball at Kapatiran" },
+//   { id: "3", title: "Eat Hard Boiled Eggs" },
+//   { id: "4", title: "Basketball at San Mateo" },
+//   { id: "5", title: "Sleep @9pm" },
+//   { id: "6", title: "Wake up @10am" },
+//   { id: "7", title: "Wake up @11am" },
+// ];
 
 function App() {
-  // const dummyTasks = [
-  //   { id: "1", title: "Buy Mom and Dad gifts" },
-  //   { id: "2", title: "Basketball at Kapatiran" },
-  //   { id: "3", title: "Eat Hard Boiled Eggs" },
-  //   { id: "4", title: "Basketball at San Mateo" },
-  //   { id: "5", title: "Sleep @9pm" },
-  //   { id: "6", title: "Wake up @10am" },
-  //   { id: "7", title: "Wake up @11am" },
-  // ];
-
   const [name, setName] = useState("");
-  const [list, setList] = useState(
-    getLocalStorage().sort((a, b) => {
-      if (a.completed === b.completed) {
-        return a.title.localeCompare(b.title);
-      }
-      return a.completed ? -1 : 1;
-    })
-  );
-
+  const [list, setList] = useState(getLocalStorage());
   const [alert, setAlert] = useState({ show: false, type: "", msg: "" });
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
+  const [sortType, setSortType] = useState("default");
+
+  useEffect(() => {
+    const sortList = () => {
+      let sortedList = [...list];
+      switch (sortType) {
+        case "default":
+          sortedList.sort((a, b) => parseInt(a.id) - parseInt(b.id));
+          break;
+        case "alphabetical":
+          sortedList.sort((a, b) => a.title.localeCompare(b.title));
+          break;
+        case "completeTop":
+          sortedList.sort((a, b) => b.completed - a.completed);
+          break;
+        case "incompleteTop":
+          sortedList.sort((a, b) => a.completed - b.completed);
+          break;
+
+        default:
+          break;
+      }
+      return sortedList;
+    };
+
+    setList(sortList());
+  }, [sortType, list]);
 
   const showAlert = (show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
+
+    clearTimeout(window.alertTimeout);
+
+    if (show) {
+      window.alertTimeout = setTimeout(() => {
+        setAlert({ show: false, type: "", msg: "" });
+      }, 3000);
+    }
   };
 
   const removeItem = (id) => {
@@ -113,36 +139,31 @@ function App() {
       if (isEditing) {
         tempTasks = list.map((item) => {
           if (item.id === editID) {
-            return { ...item, title: name };
+            return {
+              ...item,
+              title: name,
+              id: item.id,
+              completed: item.completed,
+            };
           }
           return item;
         });
         setIsEditing(false);
+        setEditID(null);
       } else {
         const newTask = {
           id: new Date().getTime().toString(),
           title: name,
           completed: false,
         };
-        tempTasks = [...list, newTask];
+        tempTasks = [newTask, ...list];
       }
 
-      tempTasks.sort((a, b) => {
-        if (a.completed === b.completed) {
-          return a.title.localeCompare(b.title);
-        }
-        return a.completed ? -1 : 1;
-      });
-
-      setList(tempTasks);
       setName("");
       showAlert(true, "success", isEditing ? "Task updated" : "Task added");
+      setList(tempTasks);
     }
   };
-
-  useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(list));
-  }, [list]);
 
   return (
     <section className="section-center">
@@ -154,9 +175,23 @@ function App() {
         isEditing={isEditing}
       />
 
+      <div className="sort-container">
+        <label htmlFor="sort">Sort by: </label>
+        <select
+          id="sort"
+          value={sortType}
+          onChange={(e) => setSortType(e.target.value)}
+        >
+          <option value="default">Default</option>
+          <option value="alphabetical">Alphabetical</option>
+          <option value="completeTop">Completed Tasks at Top</option>
+          <option value="incompleteTop">Incomplete Tasks at Top</option>
+        </select>
+      </div>
       {alert.show && (
         <Alert {...alert} removeAlert={() => showAlert()} list={list} />
       )}
+
       {list.length > 0 && (
         <div className="grocery-container">
           <List
@@ -165,13 +200,13 @@ function App() {
             editItem={editItem}
             checkItem={checkItem}
           />
-
           <button onClick={clearCompletedTasks} className="clear-btn">
             Remove All Completed Tasks
           </button>
           <button className="clear-btn" onClick={clearList}>
             remove all tasks
           </button>
+
           <Footer list={list} />
         </div>
       )}
